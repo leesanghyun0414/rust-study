ec70d10678f888635f74e0fcca234117ae228ed0
sanghyun.lee <lee.sanghyun0414@gmail.com>
2022-12-15 19:47:43 +0900

    Add actix-web tutorial code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("hello world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+    HttpServer::new(||{
+        App::new()
+            .service(hello)
+            .route("/hey", web::get().to(manual_hello))
+    })
+        .bind(("app", 8080))?
+        .run()
+        .await
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello World!!!!")
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
 }
